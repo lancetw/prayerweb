152ce5d7cc238909243fa852c053fba4e8157093
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-14 11:12:00 +0800

    fix params

--- app/controllers/DataController.php
+++ app/controllers/DataController.php
@@ -362,19 +362,19 @@ class DataController extends \BaseController {
     $vd = Validator::make($in, $rules);
     if($vd->fails()) return;
 
-    $church = Church::whereHas('targets', function ($q) {
+    $church = Church::whereHas('targets', function ($q) use($in) {
       $q->where('sinner', '=', true)
         ->where('qlink', $in['qlink']);
     })->first();
     $sinners = $church->targets()->count();
 
-    $church = Church::whereHas('targets', function ($q) {
+    $church = Church::whereHas('targets', function ($q) use($in) {
       $q->where('baptized', '=', true)
         ->where('qlink', $in['qlink']);
     })->first();
     $baptizeds = $church->targets()->count();
 
-    $church = Church::whereHas('targets', function ($q) {
+    $church = Church::whereHas('targets', function ($q) use($in) {
       $q->where('metter', '=', true)
         ->where('qlink', $in['qlink']);
     })->first();
