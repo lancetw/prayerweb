cc373f5a6fd0c400ab6f7cb19767126bdfc1c6f5
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-17 16:27:49 +0800

    test

--- app/models/Church.php
+++ app/models/Church.php
@@ -32,13 +32,13 @@ class Church extends \Eloquent {
 
   public function targets()
   {
-    return $this->hasManyThrough('Target', 'user_churches', 'cid', 'uid');
+    return $this->hasManyThrough('Target', 'UserChurch', 'cid', 'uid');
   }
 
 
   public function busteds()
   {
-    return $this->hasManyThrough('Busted', 'user_churches', 'cid', 'uid');
+    return $this->hasManyThrough('Busted', 'UserChurch', 'cid', 'uid');
   }
 
 
