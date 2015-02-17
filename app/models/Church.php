dcae79a004524a05bf4131fb52889732cf2fbe1d
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-17 15:51:03 +0800

    try to fix

--- app/models/Church.php
+++ app/models/Church.php
@@ -32,13 +32,13 @@ class Church extends \Eloquent {
 
   public function targets()
   {
-    return $this->belongsToMany('Target', 'user_churches', 'uid');
+    return $this->belongsToMany('Target', 'user_churches', 'tid');
   }
 
 
   public function busteds()
   {
-    return $this->belongsToMany('Busted', 'user_churches', 'uid');
+    return $this->belongsToMany('Busted', 'user_churches', 'tid');
   }
 
 
