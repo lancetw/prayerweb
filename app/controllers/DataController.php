5d4daea78aab3092ab4d89fe60b5fa3e4e564ee7
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-17 19:32:29 +0800

    test

--- app/controllers/DataController.php
+++ app/controllers/DataController.php
@@ -337,7 +337,7 @@ class DataController extends \BaseController {
 
     $church = Church::where('qlink', $in['qlink'])->first();
     if ($church) {
-      $rdata = $church->busteds->orderBy('created_at', 'DESC')->get()->each(function ($item) {
+      $rdata = $church->busteds()->orderBy('created_at', 'DESC')->get()->each(function ($item) {
         $item->setHidden(['cid', 'id', 'uid', 'name', 'deleted_at', 'updated_at']);
       })->toArray();
 
