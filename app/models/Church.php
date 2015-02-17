4cd161f519dddef5d0cc0f8cf9f3724ddb9ff3e8
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-17 15:12:04 +0800

    fix typo

--- app/models/Church.php
+++ app/models/Church.php
@@ -32,13 +32,13 @@ class Church extends \Eloquent {
 
   public function targets()
   {
-    return $this->hasManyThrough('Target', 'UserChurch', 'cid', 'uid');
+    return $this->hasManyThrough('Target', 'UserChurch', 'uid', 'uid');
   }
 
 
   public function busteds()
   {
-    return $this->hasManyThrough('Busted', 'UserChurch', 'cid', 'uid');
+    return $this->hasManyThrough('Busted', 'UserChurch', 'uid', 'uid');
   }
 
 
