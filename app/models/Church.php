df2cb036a1bd440fd5328554dc03dd604a4cdb3e
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-23 07:55:16 +0800

    Revert "fix hasManyThrough"
    This reverts commit 4feb7d7d59175a00085b567d2a9817ef8b284dc6.

--- app/models/Church.php
+++ app/models/Church.php
@@ -31,12 +31,12 @@ class Church extends \Eloquent {
 
   public function targets()
   {
-    return $this->hasManyThrough('Target', 'Church', 'cid', 'uid');
+    return $this->hasManyThrough('Target', 'UserChurch', 'cid', 'uid');
   }
 
   public function busteds()
   {
-    return $this->hasManyThrough('Busted', 'Church', 'cid', 'uid');
+    return $this->hasManyThrough('Busted', 'UserChurch', 'cid', 'uid');
   }
 
   public function newCollection(array $models = array())
