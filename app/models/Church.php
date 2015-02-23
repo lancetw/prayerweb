587d429a6455c11c299722c0ae90b85d55cb9a1f
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-23 08:58:03 +0800

    fix

--- app/models/Church.php
+++ app/models/Church.php
@@ -27,7 +27,7 @@ class Church extends \Eloquent {
 
   public function targets()
   {
-    return $this->belongsToMany('Target', 'user_churches', 'uid', 'cid')->withPivot('uid', 'cid');
+    return $this->belongsToMany('Target', 'user_churches', 'uid', 'cid');
   }
 
 
